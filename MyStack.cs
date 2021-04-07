7781c9f2c72eb4508740a73165e001d614e0caf8
runningstone <runningstone@yandex.ru>
2021-04-07 10:09:29 +0300

    Stack started

---
+++ MyStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+    class MyStack<T>
+    {
+        public int Count { get; private set; }
+        public MyStack()
+        {
+            Count = 0;
+        }
+        public int Pop()
+        {
+            return 0;
+        }
+        public void Push()
+        {
+
+        }
+        private void Remove()
+        {
+
+        }
+        public void Peek()
+        {
+
+        }
+        private class Node
+        {
+            public int Value { get; private set; }
+
+            private Node()
+            {
+
+            }
+        }
+    }
+}
