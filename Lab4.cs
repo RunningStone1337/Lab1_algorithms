71ebece84fb511b6e84438555747735a59c121a1
runningstone <runningstone@yandex.ru>
2021-04-08 12:36:11 +0300

    Deque modified 1 task done

--- Lab4.cs
+++ Lab4.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using System.Collections;
+using System.IO;
 
 namespace Lab4
 {
@@ -26,17 +27,71 @@ namespace Lab4
 #if deque
             #region Deque
             MyDeque<int> deque = new MyDeque<int>();
-            deque.Enqueue(0);
-            deque.Enqueue(1);
-            deque.Enqueue(2);
-            deque.Dequeue(-1);
-            var first = deque.RemoveFirst();
-            var last = deque.RemoveLast();
+            deque.Enqueue(5);
+            deque.Dequeue(4);
             var head = deque.PopHead();
             var tail = deque.PopTail();
-
+            var first = deque.RemoveFirst();
+            var last = deque.RemoveLast();
             #endregion
 #endif
+            #region Tasks
+            MyDeque<string> deq1 = new MyDeque<string>();
+            string filename = "Books.txt";
+            string dir = Directory.GetCurrentDirectory();
+            while (!File.Exists(dir + Path.DirectorySeparatorChar + filename))
+            {
+                dir = dir.Substring(0, dir.LastIndexOf('\\'));
+            }
+            using (StreamReader reader = new StreamReader(dir + Path.DirectorySeparatorChar + filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    deq1.Enqueue(line);
+                }
+            }
+            var t1res = Task1(deq1);
+            foreach (var item in t1res)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+        }
+        /*
+         Отсортировать строки файла, содержащие названия книг, в алфавитном порядке с
+        использованием двух деков.
+         */
+        public static MyDeque<string> Task1(MyDeque<string> input)
+        {
+            var first = new MyDeque<string>();
+            var second = new MyDeque<string>();
+            first.Dequeue(input.RemoveFirst());
+            while (input.Count > 0)
+            {
+                var temp = input.RemoveFirst();
+                if (temp.CompareTo(first.PopHead())<=0)///если temp должен быть перед первым
+                {
+                    first.Dequeue(temp);//ставим в первый дек в начало
+                }
+                else if(temp.CompareTo(first.PopTail())<0)///если temp должен быть перед последним
+                {
+                    while (temp.CompareTo(first.PopTail())<=0)//пока temp не будет >= first.Tail
+                    {
+                        second.Enqueue(first.RemoveLast());
+                    }
+                    first.Enqueue(temp);
+                    while (second.Count>0)
+                    {
+                        first.Enqueue(second.RemoveLast());
+                    }
+                }
+                else
+                {
+                    first.Enqueue(temp);
+                }
+            }
+            return first;
         }
     }
 }
